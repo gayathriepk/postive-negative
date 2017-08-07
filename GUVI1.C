e38eba34e2b719323a72a65ba3c02136eb405449
gayathriepk <gayathriepk18@gmail.com>
2017-08-07 10:10:35 +0530

    Add files via upload

---
+++ GUVI1.C
@@ -0,0 +1,21 @@
+void main()
+{
+int n;
+printf("Enter the number");
+scanf(" %d",&n);
+if(n!=0)
+ {
+ if(n%2==0)
+  {
+   printf("Its even");
+  }
+  else
+  {
+   printf("Its odd");
+  }
+  }
+ else
+ {
+  printf("Its 0");
+ }
+}
