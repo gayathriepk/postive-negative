71890f777912162295e55c9e00e789a48ff7a6c0
gayathriepk <gayathriepk18@gmail.com>
2017-08-07 10:04:23 +0530

    Add files via upload

---
+++ GUVI.C
@@ -0,0 +1,21 @@
+void main()
+{
+float n;
+printf("Enter the number");
+scanf(" %f",&n);
+if(n!=0)
+ {
+ if(n<0)
+  {
+   printf("Its negative");
+  }
+  else
+  {
+   printf("Its postive");
+  }
+  }
+ else
+ {
+  printf("Its 0");
+ }
+}
